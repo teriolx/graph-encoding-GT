1895b62c85ed965f9220521014961a54a10c8e41
Terezia Olsiakova <e12331438@student.tuwien.ac.at>
2025-05-31 13:48:33 +0200

    do only LPCA encoding for CIFAR

--- GraphGPS/graphgps/encoder/lpca_encoder.py
+++ GraphGPS/graphgps/encoder/lpca_encoder.py
@@ -11,13 +11,17 @@ class LPCAEncoder(torch.nn.Module):
         self.enc_dim = cfg.ctenc_LPCAEnc.dim_ct
         self.emb_dim = emb_dim
         self.pass_as_var = cfg.ctenc_LPCAEnc.pass_as_var if hasattr(cfg.ctenc_LPCAEnc, "pass_as_var") else False
+        self.concat_x = cfg.ctenc_LPCAEnc.concat_x if hasattr(cfg.ctenc_LPCAEnc, "concat_x") else True
 
 
     def forward(self, batch):
         lpca_enc = getattr(batch, 'lpca_enc')
 
         if self.enc_dim > 0:
-            batch.x = torch.cat((batch.x, lpca_enc), 1)
+            if self.concat_x:
+                batch.x = torch.cat((batch.x, lpca_enc), 1)
+            else:
+                batch.x = lpca_enc
             print(self.emb_dim)
             print(batch.x.shape[1])
             assert batch.x.shape[1] == self.emb_dim
